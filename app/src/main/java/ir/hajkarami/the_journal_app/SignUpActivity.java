833778b788aa0c36428de8615d6db9652e42fe70
mshajkarami <mshajkarami.dev@gmail.com>
2025-04-09 12:40:55 +0330

    Implement user signup functionality with email and password authentication.

--- app/src/main/java/ir/hajkarami/the_journal_app/SignUpActivity.java
+++ app/src/main/java/ir/hajkarami/the_journal_app/SignUpActivity.java
@@ -1,10 +1,19 @@
 package ir.hajkarami.the_journal_app;
+
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -12,11 +21,10 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
-
 public class SignUpActivity extends AppCompatActivity {
     // Widgets
     EditText password_create, username_create, email_create;
-    Button createBTN ;
+    Button createBTN;
 
     // Firebase Auth
     private FirebaseAuth mFirebaseAuth;
@@ -47,12 +55,45 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 currentUser = firebaseAuth.getCurrentUser();
-                if ( currentUser != null){
+                if (currentUser != null) {
                     // user Already logged in
-                }else {
+                } else {
                     // user not logged in
                 }
             }
         };
+        createBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (
+                        !TextUtils.isEmpty(email_create.getText().toString())
+                                && !TextUtils.isEmpty(password_create.getText().toString())
+                                && !TextUtils.isEmpty(username_create.getText().toString())
+                ){
+                    String email = email_create.getText().toString();
+                    String pass = password_create.getText().toString();
+                    String username = username_create.getText().toString();
+                    CreateUserEmailAccount(email, pass, username);
+                }
+            }
+        });
+    }
+
+    private void CreateUserEmailAccount(
+            String email,
+            String pass,
+            String username
+    ) {
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass) && !TextUtils.isEmpty(username)) {
+            mFirebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // the user is create successfully
+                        Toast.makeText(SignUpActivity.this, "Account is create successfully", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
