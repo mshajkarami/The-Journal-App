b1ed52d737e51d978cf24603f777af32a2051dcd
mshajkarami <mshajkarami.dev@gmail.com>
2025-04-09 12:07:05 +0330

    Add Firebase Auth and Firestore dependencies, implement signup UI and basic auth listener.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
-    id("com.google.gms.google-services")
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.3" apply false
+    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
 }
 
 android {
@@ -38,6 +40,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/ir/hajkarami/the_journal_app/SignUpActivity.java
+++ app/src/main/java/ir/hajkarami/the_journal_app/SignUpActivity.java
@@ -1,24 +1,58 @@
 package ir.hajkarami.the_journal_app;
-
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 
 public class SignUpActivity extends AppCompatActivity {
+    // Widgets
+    EditText password_create, username_create, email_create;
+    Button createBTN ;
+
+    // Firebase Auth
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseUser currentUser;
+
+    // Firebase Connection
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference mCollectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        createBTN = findViewById(R.id.acc_signUp_btn);
+        password_create = findViewById(R.id.password_create);
+        username_create = findViewById(R.id.username_create_ET);
+        email_create = findViewById(R.id.email_create);
+
+        // Fire Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        // Listening for changes in the authentication
+        // state and responds accordingly when the
+        // the state changes
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+                if ( currentUser != null){
+                    // user Already logged in
+                }else {
+                    // user not logged in
+                }
+            }
+        };
     }
 }
